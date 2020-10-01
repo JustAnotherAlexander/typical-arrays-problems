3fad34066adde8415ef73b1fa3fe3d0f2fb58bc5
Aliaksandr Matveyankou <aleksandreliuz@gmail.com>
2020-10-01 21:35:14 +0300

    feat: add finding the min and max values in array

--- src/index.js
+++ src/index.js
@@ -11,11 +11,22 @@ exports.min = function min (array) {
 			min=array[i];
 		}
 	}
-
+	return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (array==undefined || array.length==0) {
+		return 0;
+	}
+
+	let max = array[0];
+
+	for (let i=0; i<array.length; i++) {
+		if (array[i]>max) {
+			max=array[i];
+		}
+	}
+	return max;
 }
 
 exports.avg = function avg (array) {
